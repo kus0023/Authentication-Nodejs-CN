0894fa5769b499ac0561860e684f56685334f727
Kumar Utsav Singh <utsav.singh0023@gmail.com>
2023-08-15 23:41:09 +0530

    home page created with form and route for reset password.

--- src/routes/home.js
+++ src/routes/home.js
@@ -2,9 +2,23 @@ const router = require('express').Router();
 
 const passport = require('passport');
 
+const User = require('../models/User');
+
 router.get('/', passport.checkAuthentication, function(req, res){
+
+    const {firstName, secondName, email, _id} = req.user;
     
-    return res.render('home');
+    return res.render('home', {firstName, secondName, email, userid: _id});
+})
+
+router.post('/reset-password', passport.checkAuthentication, function(req, res){
+
+    try {
+        console.log(req.user);
+    } catch (error) {
+        
+    }
+
 })
 
 
