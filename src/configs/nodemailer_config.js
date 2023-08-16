b4ecdf453cdae141a670ea471981fa29cc314680
Kumar Utsav Singh <utsav.singh0023@gmail.com>
2023-08-16 20:31:02 +0530

    Mailer config done

---
+++ src/configs/nodemailer_config.js
@@ -0,0 +1,39 @@
+const nodemailer = require('nodemailer');
+const ejs = require('ejs');
+const path = require('path');
+
+const transporter = nodemailer.createTransport({
+    service: process.env.NODEMAILER_SERVICE,
+    host: process.env.NODEMAILER_HOST,
+    port: process.env.NODEMAILER_PORT,
+    secure: false,
+    auth: {
+      user: process.env.NODEMAILER_USER,
+      pass: process.env.NODEMAILER_PASS
+    }
+  });
+
+let renderTemplate = (data, relativePath)=>{
+
+    let mailHtml;
+
+    ejs.renderFile(
+        path.join(__dirname, '../views/mailers', relativePath),
+        data,
+        function(err, template){
+            if(err){
+                console.log('error in rendering: ', err);
+                return;
+            }
+
+            mailHtml = template;
+        }
+    )
+
+    return mailHtml;
+}
+
+module.exports = {
+    transporter,
+    renderTemplate
+}
